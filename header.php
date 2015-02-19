3666ee23e81a8ec2bbae50ad219ef126952d63b4
Reinier Ladan <reinierl@gmail.com>
2015-02-19 15:45:05 +0100

    Changed tagline

--- header.php
+++ header.php
@@ -71,5 +71,5 @@
 	<div id="page-wrap">
 		<div id="header">
 			<h1 id="logo"><a href="<?php echo get_option('home'); ?>/"><span><?php bloginfo('name'); ?></span></a></h1>
-			<div class="description">Stukje duiding bij het leven van <a rel="me" href="/over-sinds82-en-reinier-ladan" class="link-over">Reinier Ladan</a>.</div>
+			<div class="description">Openbare hersenspinsels van <a rel="me" href="/over-sinds82-en-reinier-ladan" class="link-over">Reinier Ladan</a>.</div>
 		</div>
