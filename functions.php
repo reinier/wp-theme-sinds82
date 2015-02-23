3adef204b7b3d5564929c46ea68a98c86c5d5a30
Reinier Ladan <reinierl@gmail.com>
2015-02-23 11:52:30 +0100

    Added support for custom post formats

--- functions.php
+++ functions.php
@@ -1,24 +1,29 @@
 <?php
+
 	// Clean up the <head>
 	function removeHeadLinks() {
-    	remove_action('wp_head', 'rsd_link');
-    	remove_action('wp_head', 'wlwmanifest_link');
-    }
-    
-    add_action('init', 'removeHeadLinks');
-    remove_action('wp_head', 'wp_generator');
-    
-    if (function_exists('register_sidebar')) {
-    	register_sidebar(array(
-    		'name' => 'Sidebar Widgets',
-    		'id'   => 'sidebar-widgets',
-    		'description'   => 'These are widgets for the sidebar.',
-    		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-    		'after_widget'  => '</div>',
-    		'before_title'  => '<h2>',
-    		'after_title'   => '</h2>'
-    	));
-    }
+  	remove_action('wp_head', 'rsd_link');
+  	remove_action('wp_head', 'wlwmanifest_link');
+  }
+
+  add_action('init', 'removeHeadLinks');
+  remove_action('wp_head', 'wp_generator');
+
+	add_theme_support( 'post-formats', array(
+		'image', 'video', 'quote', 'link', 'audio'
+	) );
+
+  if (function_exists('register_sidebar')) {
+  	register_sidebar(array(
+  		'name' => 'Sidebar Widgets',
+  		'id'   => 'sidebar-widgets',
+  		'description'   => 'These are widgets for the sidebar.',
+  		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+  		'after_widget'  => '</div>',
+  		'before_title'  => '<h2>',
+  		'after_title'   => '</h2>'
+  	));
+  }
 
 	function permalink_links_to_rss($content) {
 		global $wp_query;
@@ -33,7 +38,7 @@
 		}
 		return $content;
 	}
-	
+
 	function clicktrough_as_rss_link($permalink) {
 		global $wp_query;
 		if($url = get_post_meta($wp_query->post->ID, 'clicktrough', true)) {
@@ -41,7 +46,7 @@
 		}
 		return $permalink;
 	}
-	
+
 	add_filter('the_excerpt_rss', 'permalink_links_to_rss');
 	add_filter('the_content', 'permalink_links_to_rss');
 	add_filter('the_permalink_rss', 'clicktrough_as_rss_link');
